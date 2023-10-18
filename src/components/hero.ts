7a11fdbabae4e7eca3ffb8cbfff78347be0312af
Leighton Grant <oneal.grant@outlook.com>
2023-10-18 18:03:05 +0100

    adds hero and styling

---
+++ src/components/hero.ts
@@ -0,0 +1,12 @@
+import banner from "/images/banner.jpg";
+const hero = `
+    <section id='hero'>
+        <img src='${banner}' alt='banner'>
+        <div>
+            <h1>Learn something new everyday</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button type='button'>Start here</button>
+        </div>
+    </section>
+`;
+export default hero;
--- src/components/layout.ts
+++ src/components/layout.ts
@@ -1,6 +1,8 @@
 import "./components.css";
 import header from "./header";
+import hero from "./hero";
 const layout = `
     ${header}
+    ${hero}
 `;
 export default layout;
