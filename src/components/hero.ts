34ad9b5798d8c007f743d80bf031de7c53380291
leightongrant <oneal.grant@outlook.com>
2023-10-19 20:51:29 +0100

    refactors styling

--- src/components/hero.ts
+++ src/components/hero.ts
@@ -1,8 +1,10 @@
 import banner from "/images/banner.jpg";
 const hero = `
     <section id='hero'>
-        <img src='${banner}' alt='banner'>
-        <div>
+        <div id='hero-ftc'>
+            <img src='${banner}' alt='banner'>
+        </div>
+        <div id='hero-sdc'>
             <h1>Learn something new everyday</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <button type='button'>Start here</button>
--- src/components/info.ts
+++ src/components/info.ts
@@ -5,7 +5,7 @@ import infoGuest from "/images/information-guest-lecture.jpg";
 
 const info = `
     <section id='info'>
-        <div>
+        <div id='info-ftc'>
             <img src='${infoMain}' alt='Emanuel Sr Strategist'>
             <h2>It doesn't hurt to keep practicing</h2>
             <p>
@@ -14,29 +14,35 @@ const info = `
                 Vel dolor ut, error ab cumque enim doloremque unde dicta omnis sequi. 
                 Eos reprehenderit mollitia modi numquam unde omnis quidem ratione atque.
             </p>
-            <p>
+            <p id='strategist'>
                 Emanuel, Sr Strategist at Hiring.com
             </p>
         </div>
-        <div>
-            <div>
-                <img src='${infoOrientation}' alt='Orientation'>
+        <div id='info-sdc'>
+            <div class='info-sdc-c'>
+                <div class='info-img'>
+                    <img src='${infoOrientation}' alt='Orientation'>
+                </div>
                 <div>
                     <h3>Orientation date</h3>
                     <p>Tue 10/11 & Wed 10/12: 8am-3pm</p>
                     <a href='#'>Read more</a>
                 </div>
             </div>
-            <div>
-                <img src='${infoCampus}' alt='The campus'>
+            <div class='info-sdc-c'>
+                <div class='info-img'>
+                    <img src='${infoCampus}' alt='The campus'>
+                </div>
                 <div>
                     <h3>Our campus</h3>
                     <p>Find which campus is close by you</p>
                     <a href='#'>Read more</a>
                 </div>
             </div>
-            <div>
-                <img src='${infoGuest}' alt='Oliver Sack'>
+            <div class='info-sdc-c'>
+                <div class='info-img'>
+                    <img src='${infoGuest}' alt='Oliver Sack'>
+                </div>
                 <div>
                     <h3>Our guest lecture</h3>
                     <p>Join a keynote with Oliver Sack about music in medical treatment</p>
