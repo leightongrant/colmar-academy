a48d10af88d703bdcb42d2b1b889a101305a5a67
Leighton Grant <oneal.grant@outlook.com>
2023-10-20 14:14:37 +0100

    adds responsive styling to info section

--- src/components/info.ts
+++ src/components/info.ts
@@ -1,8 +1,32 @@
 import infoMain from "/images/information-main.jpg";
 import infoOrientation from "/images/information-orientation.jpg";
+import infoOrientationM from "/images/information-orientation-mobile.jpg";
 import infoCampus from "/images/information-campus.jpg";
+import infoCampusM from "/images/information-campus-mobile.jpg";
 import infoGuest from "/images/information-guest-lecture.jpg";
+import infoGuestM from "/images/information-guest-lecture-mobile.jpg";
 
+window.addEventListener("load", () => {
+  let infoOrient = <HTMLDivElement>document.getElementById("orientation");
+  let infoCamp = <HTMLDivElement>document.getElementById("campus");
+  let infoGues = <HTMLDivElement>document.getElementById("guest");
+  window.addEventListener("resize", () => {
+    infoOrient.setAttribute(
+      "src",
+      `${window.innerWidth < 576 ? infoOrientationM : infoOrientation}`
+    );
+
+    infoCamp.setAttribute(
+      "src",
+      `${window.innerWidth < 576 ? infoCampusM : infoCampus}`
+    );
+
+    infoGues.setAttribute(
+      "src",
+      `${window.innerWidth < 576 ? infoGuestM : infoGuest}`
+    );
+  });
+});
 const info = `
     <section id='info'>
         <div id='info-ftc'>
@@ -20,10 +44,10 @@ const info = `
         </div>
         <div id='info-sdc'>
             <div class='info-sdc-c'>
-                <div class='info-img'>
-                    <img src='${infoOrientation}' alt='Orientation'>
+                <div class='info-img' >
+                   <img src='${infoOrientation}' alt='Orientation' id='orientation'>
                 </div>
-                <div>
+                <div class='info-txt'>
                     <h3>Orientation date</h3>
                     <p>Tue 10/11 & Wed 10/12: 8am-3pm</p>
                     <a href='#'>Read more</a>
@@ -31,9 +55,9 @@ const info = `
             </div>
             <div class='info-sdc-c'>
                 <div class='info-img'>
-                    <img src='${infoCampus}' alt='The campus'>
+                    <img src='${infoCampus}' alt='The campus' id='campus'>
                 </div>
-                <div>
+                <div class='info-txt'>
                     <h3>Our campus</h3>
                     <p>Find which campus is close by you</p>
                     <a href='#'>Read more</a>
@@ -41,9 +65,9 @@ const info = `
             </div>
             <div class='info-sdc-c'>
                 <div class='info-img'>
-                    <img src='${infoGuest}' alt='Oliver Sack'>
+                    <img src='${infoGuest}' alt='Oliver Sack' id='guest'>
                 </div>
-                <div>
+                <div class='info-txt'>
                     <h3>Our guest lecture</h3>
                     <p>Join a keynote with Oliver Sack about music in medical treatment</p>
                     <a href='#'>Read more</a>
@@ -52,4 +76,5 @@ const info = `
         </div>
     </section>
 `;
+
 export default info;
