c2414b03e81f27278be8898794889b19d148f652
Leighton Grant <oneal.grant@outlook.com>
2023-10-21 10:57:07 +0100

    adds footer

---
+++ src/components/footer.ts
@@ -0,0 +1,13 @@
+const footer = `
+    <footer>
+        <div id='copyright'>
+            <p>© 2023 Colomar Academy. All rights reserved</p>
+        </div>
+        <div id='terms'>
+            <a href='#'>Terms</a>
+            <a href='#'>Privacy</a>
+        </div>
+    </footer>
+`;
+
+export default footer;
--- src/components/layout.ts
+++ src/components/layout.ts
@@ -3,10 +3,12 @@ import header from "./header";
 import hero from "./hero";
 import info from "./info";
 import learning from "./learning";
+import footer from "./footer";
 const layout = `
     ${header}
     ${hero}
     ${info}
     ${learning}
+    ${footer}
 `;
 export default layout;
