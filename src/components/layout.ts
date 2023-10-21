be03db7f373daac1bf339345b31ffaa2d8b5dc1c
Leighton Grant <oneal.grant@outlook.com>
2023-10-21 12:28:17 +0100

    adds thesis section

--- src/components/layout.ts
+++ src/components/layout.ts
@@ -3,12 +3,14 @@ import header from "./header";
 import hero from "./hero";
 import info from "./info";
 import learning from "./learning";
+import thesis from "./thesis";
 import footer from "./footer";
 const layout = `
     ${header}
     ${hero}
     ${info}
     ${learning}
+    ${thesis}
     ${footer}
 `;
 export default layout;
